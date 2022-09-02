4c965b3dd3b901d47396d9e67fe4e30d374df26f
Robert <rtomim05@gmail.com>
2022-09-02 11:44:57 +0200

    Work on lab 1

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,1 +1,4 @@
-Hi
+print('I am learning python arcade and I already know this part.\nNow i will have to turn in this assignment.')
+print('''Using triple quotes is a good option as well aaaaaaaaaaaaaaaaaaaaaaaaaaaa
+aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+aaaaaaaaaaaaaaaaaaaaaaaaaaaaa''')
